14c1ccd238371f68dada224b67e86f206f4b10aa
MarvinENRsu <Mawing993@gmail.com>
2022-11-04 22:21:50 +0700

    adduser

--- src/component/header.js
+++ src/component/header.js
@@ -1,13 +1,15 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import { useState, useRef } from 'react'
-import axios from 'axios'
-import { BrowserRouter as Router , Route, Link, Routes, useNavigate, redirect } from "react-router-dom";
+import { useRef } from 'react'
+// import axios from 'axios'
+// import { BrowserRouter as Router , Route, Link, Routes, useNavigate, redirect } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faStickerMule } from "@fortawesome/free-brands-svg-icons"
 
 
 function header() {
-  const navigate = useNavigate()
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+  // const navigate = useNavigate()
+  //   const [email, setEmail] = useState('')
+  //   const [password, setPassword] = useState('')
 
     const navRef = useRef();
 
@@ -15,14 +17,29 @@ function header() {
 		navRef.current.classList.toggle("responsive_nav");
 	};
 
-    return (
-        <header>
-			<h3>LOGO</h3>
+  return (
+    <header>
+			<FontAwesomeIcon className='icon-header' icon={faStickerMule}></FontAwesomeIcon>
 			<nav ref={navRef}>
-				<a href="/#">Home</a>
-				<a href="/#">My work</a>
-				<a href="/#">Blog</a>
-				<a href="/#">About me</a>
+        <div className='dropdown'>
+          Home
+          <div className="dropdown-content">
+            <div className='h1'>Analytics</div>
+                <p>Get a better understanding of where your traffic is coming from.</p>
+            <div className='h1'>Engagement</div>
+                <p>Get a better understanding of where your traffic is coming from.</p>
+            <div className='h1'>Security</div>
+                <p>Get a better understanding of where your traffic is coming from.</p>
+            <div className='h1'>Integrations</div>
+                <p>Get a better understanding of where your traffic is coming from.</p>
+            <div className='h1'>More</div>
+                <p>Get a better understanding of where your traffic is coming from.</p>
+
+          </div> 
+        </div>
+				<a href="/#">Pricing</a>
+				<a href="/#">Docs</a>
+				<a href="/#">More</a>
 				<hr className='line-header'></hr>
 				<button
 					className="nav-btn nav-close-btn"
--- src/dataEmployee.js
+++ src/dataEmployee.js
@@ -8,6 +8,9 @@ import Footer from './component/footer'
 function DataEmployee() {
   const navigate = useNavigate()
   const [dataUser, setDataUser] = useState([])
+  const [editTable, setEditTable] = useState(false)
+  const [fristname, setFristname] = useState('')
+  const [lasttname, setLastname] = useState('')
 
   useEffect(() => {
     const token = sessionStorage.getItem('token')
@@ -23,11 +26,43 @@ function DataEmployee() {
     console.log(dataUser)
   }
 
+  const onClickAdduser = async () =>{ 
+    setEditTable(true)   
+  } 
+
+  const onClickSumit = async () =>{ 
+    await axios.post('https://reqres.in/api/users', {
+            "name": fristname ,
+            "job": lasttname
+        }).then((res) => {
+          console.log(res)
+            if (res) {
+              const newData = {
+                email: res.data.id,
+                first_name: res.data.name,
+                last_name: res.data.job
+              }
+              console.log(newData)
+              dataUser.push(newData)
+
+              setEditTable(false)
+            }
+        })
+    
+
+       
+  }
+
+  
+
   
 
   return (
     <div className="App">
       <Header></Header>
+      <button className='button-add' onClick={() => onClickAdduser()}>
+        Add Users
+      </button>
         <div className="table-section">
           <table>
             <tr>
@@ -35,13 +70,52 @@ function DataEmployee() {
               <th>Fist Name</th>
               <th>Last Name</th>
             </tr>
+            {editTable && (
+              <tr className={editTable}>
+                <td>
+                  {/* <input 
+                    className='input-box' 
+                    type="text" 
+                    name="password" 
+                    placeholder="Email.."
+                    onChange={(e) => {
+                        console.log(e.target.value)
+                        setEmail(e.target.value)
+                    }}
+                  /> */}
+                </td>
+                <td><input 
+                    className='input-box' 
+                    type="text" 
+                    name="fristname" 
+                    placeholder="Frist Name.."
+                    onChange={(e) => {
+                        console.log(e.target.value)
+                        setFristname(e.target.value)
+                    }}
+                  /></td>
+                <td><input 
+                    className='input-box' 
+                    type="text" 
+                    name="lastname" 
+                    placeholder="Last Name.."
+                    onChange={(e) => {
+                        console.log(e.target.value)
+                        setLastname(e.target.value)
+                    }}
+                  /></td>
+                  <button className='button-submit' onClick={() => onClickSumit()}>Accep</button>
+              </tr>
+            )}
             {dataUser.map((val, key) => {
             return (
               <tr key={key}>
                 {/* <td>{val.id}</td> */}
                 <td>
+                  <div>
                   <img src={val.avatar} alt=""/>
                   {val.email}
+                  </div>
                 </td>
                 <td>{val.first_name}</td>
                 <td>{val.last_name}</td>
